5363b56d977267bd04d1f0c551a04f9701039607
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-02-07 22:24:31 +0700

    fix(models): remove sealed & add semicolon after fromJson

--- bricks/model/__brick__/lib/domains/models/{{name.snakeCase()}}.dart
+++ bricks/model/__brick__/lib/domains/models/{{name.snakeCase()}}.dart
@@ -4,7 +4,7 @@ part '{{name.snakeCase()}}.freezed.dart';
 part '{{name.snakeCase()}}.g.dart';
 
 @freezed
-sealed class {{name.pascalCase()}} with _${{name.pascalCase()}} {
+class {{name.pascalCase()}} with _${{name.pascalCase()}} {
   // TODO: Add your named parameters.
   const factory {{name.pascalCase()}}({
     required String id,
@@ -12,5 +12,5 @@ sealed class {{name.pascalCase()}} with _${{name.pascalCase()}} {
   }) = _{{name.pascalCase()}};
 
   factory {{name.pascalCase()}}.fromJson(Map<String, dynamic> json)
-    => _${{name.pascalCase()}}FromJson(json)
+    => _${{name.pascalCase()}}FromJson(json);
 }
