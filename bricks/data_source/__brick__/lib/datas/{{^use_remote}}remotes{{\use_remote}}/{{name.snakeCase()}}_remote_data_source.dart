43397185f334d28a1940ea1521e134725c12bf30
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-02-07 17:44:51 +0700

    feat(data_source): add data source brick

---
+++ bricks/data_source/__brick__/lib/datas/{{^use_remote}}remotes{{\use_remote}}/{{name.snakeCase()}}_remote_data_source.dart
@@ -0,0 +1,43 @@
+import 'package:fpdart/fpdart.dart';
+import 'package:injectable/injectable.dart';
+
+@lazySingleton
+abstract interface class {{name.pascalCase()}}RemoteDataSource {
+  // TODO: Please customize your methods if needed.
+  TaskEither<Failure, {{name.pascalCase()}}> get();
+  TaskEither<Failure, {{name.pascaslCase()}}> add();
+  TaskEither<Failure, {{name.pascaslCase()}}> update();
+}
+
+@LazySingleton(as: {{name.pascalCase()}}RemoteDataSource)
+final class {{name.pascalCase()}}RemoteDataSourceImpl implements {{name.pascalCase()}}RemoteDataSource {
+  const {{name.pascalCase()}}RemoteDataSourceImpl(this._client);
+
+  final DioClient _client;
+
+  @override
+  TaskEither<Failure, {{name.pascalCase()}}> get() {
+    return _client.get(
+      '', // TODO: Add your path URL.
+      (resp) => {{name.pascalCase()}}.fromJson(resp['data']),
+    )
+  }
+
+  @override
+  TaskEither<Failure, {{name.pascalCase()}}> add() {
+    return _client.post(
+      '', // TODO: Add your path URL.
+      (resp) => {{name.pascalCase()}}.fromJson(resp['data']),
+      data: {}
+    )
+  }
+
+  @override
+  TaskEither<Failure, {{name.pascalCase()}}> update() {
+    return _client.patch(
+      '', // TODO: Add your path URL.
+      (resp) => {{name.pascalCase()}}.fromJson(resp['data']),
+      data: {}
+    )
+  }
+}
---
+++ bricks/data_source/hooks/post_gen.dart
@@ -0,0 +1,33 @@
+import 'package:mason/mason.dart';
+
+void run(HookContext context) async {
+  _runAddDependecies;
+  _runDartFormat;
+  _runDartFix;
+}
+
+void _runAddDependencies(HookContext context) async {
+  final progress = context.logger.progress('Adding dependencies...');
+
+  await Process.run('flutter', ['pub', 'add', 'injectable']);
+  await Process.run('flutter', ['pub', 'add', '--dev', 'build_runner']);
+  await Process.run('flutter', ['pub', 'add', '--dev', 'injectable_generator']);
+
+  progress.compete();
+}
+
+void _runDartFormat(HookContext context) async {
+  final progress = context.logger.progress('Running dart format...');
+
+  await Process.run('dart', ['format', '.']);
+
+  progress.complete();
+}
+
+void _runDartFix(HookContext context) async {
+  final progress = context.logger.progress('Applying dart fix...');
+
+  await Process.run('dart', ['fix', '--apply']);
+
+  progress.complete();
+}
