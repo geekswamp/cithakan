5ff282b2f112606ae40c51b9cebfb2a74f90acc7
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-02-08 11:58:18 +0700

    feat(hooks): add fpdart, injectable, build_runner, injectable_generator

--- bricks/network/hooks/post_gen.dart
+++ bricks/network/hooks/post_gen.dart
@@ -10,6 +10,10 @@ void _runAddDependencies(HookContext context) async {
   final progress = context.logger.progress('Adding dependencies...');
 
   await Process.run('flutter', ['pub', 'add', 'dio']);
+  await Process.run('flutter', ['pub', 'add', 'fpdart']);
+  await Process.run('flutter', ['pub', 'add', 'injectable']);
+  await Process.run('flutter', ['pub', 'add', '--dev', 'build_runner']);
+  await Process.run('flutter', ['pub', 'add', '--dev', 'injectable_generator']);
 
   progress.compete();
 }
