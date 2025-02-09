60aa23d5c2359f11440faedac95da2e52e233a62
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-02-09 20:51:24 +0700

    feat(bricks): add modules brick

---
+++ bricks/modules/__brick__/lib/cores/modules.dart
@@ -0,0 +1,51 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:injectable/injectable.dart';
+
+import 'env/env.dart';
+import 'network/dio_client.dart'
+import 'network/network_interceptor.dart';
+import 'secure_storage/secure_storage.dart';
+
+/// A module that will be used to inject dependencies.
+@module
+abstract class Modules {
+  /// [FlutterSecureStorage] instance.
+  @lazySingleton
+  FlutterSecureStorage get storage => const FlutterSecureStorage(
+    aOptions: AndroidOptions(
+      encryptedSharedPreferences: true,
+      sharedPreferencesName: kStorageName,
+    ),
+    iOptions: IOSOptions(
+      accountName: kStorageName,
+      accessibility: KeychainAccessibility.first_unlock_this_device,
+    ),
+  );
+
+  /// [Dio] instance.
+  @lazySingleton
+  Dio dio(NetworkInterceptor networkInterceptor) {
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: !kReleaseMode ? Env.baseUrlDev : Env.baseUrl,
+        connectTimeout: kDuration,
+        receiveTimeout: kDuration,
+        sendTimeout: kDuration,
+        contentType: Headers.jsonContentType,
+        followRedirects: false,
+        validateStatus: (status) => status! < 500,
+      ),
+    );
+
+    dio.interceptors.add(networkInterceptor);
+    if (!kReleaseMode) {
+      dio.interceptors.add(
+        LogInterceptor(requestBody: true, responseBody: true),
+      );
+    }
+
+    return dio;
+  }
+}
---
+++ bricks/modules/hooks/post_gen.dart
@@ -0,0 +1,35 @@
+import 'package:mason/mason.dart';
+
+void run(HookContext context) async {
+  _runAddDependencies;
+  _runDartFormat;
+  _runDartFix;
+}
+
+void _runAddDependencies(HookContext context) async {
+  final progress = context.logger.progress('Adding dependencies...');
+
+  await Process.run('flutter', ['pub', 'add', 'dio']);
+  await Process.run('flutter', ['pub', 'add', 'flutter_secure_storage']);
+  await Process.run('flutter', ['pub', 'add', 'injectable']);
+  await Process.run('flutter', ['pub', 'add', '--dev', 'build_runner']);
+  await Process.run('flutter', ['pub', 'add', '--dev', 'injectable_generator']);
+
+  progress.compete();
+}
+
+void _runDartFormat(HookContext context) async {
+  final progress = context.logger.progress('Running dart format...');
+
+  await Process.run('dart', ['format', '.']);
+
+  progress.complete();
+}
+
+void _runDartFix(HookContext context) async {
+  final progress = context.logger.progress('Applying dart fix...');
+
+  await Process.run('dart', ['fix', '--apply']);
+
+  progress.complete();
+}
