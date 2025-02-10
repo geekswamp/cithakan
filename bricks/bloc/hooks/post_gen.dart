2c03fed489b7da29a6581334ab40635c8f883d9c
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-02-10 15:49:29 +0700

    fix(hooks): change to `Future<void>` for each methods

--- bricks/bloc/hooks/post_gen.dart
+++ bricks/bloc/hooks/post_gen.dart
@@ -3,12 +3,12 @@ import 'dart:io';
 import 'package:mason/mason.dart';
 
 void run(HookContext context) async {
-  _runAddDependencies(context);
-  _runDartFormat(context);
-  _runDartFix(context);
+  await _runAddDependencies(context);
+  await _runDartFormat(context);
+  await _runDartFix(context);
 }
 
-void _runAddDependencies(HookContext context) async {
+Future<void> _runAddDependencies(HookContext context) async {
   final progress = context.logger.progress('Adding dependencies...');
 
   await Process.run('flutter', ['pub', 'add', 'flutter_bloc']);
@@ -16,7 +16,7 @@ void _runAddDependencies(HookContext context) async {
   progress.complete();
 }
 
-void _runDartFormat(HookContext context) async {
+Future<void> _runDartFormat(HookContext context) async {
   final progress = context.logger.progress('Running dart format...');
 
   await Process.run('dart', ['format', '.']);
@@ -24,7 +24,7 @@ void _runDartFormat(HookContext context) async {
   progress.complete();
 }
 
-void _runDartFix(HookContext context) async {
+Future<void> _runDartFix(HookContext context) async {
   final progress = context.logger.progress('Applying dart fix...');
 
   await Process.run('dart', ['fix', '--apply']);
