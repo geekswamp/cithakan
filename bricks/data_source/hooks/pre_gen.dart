83ee3b661931fa1237496a1912042617cea21bc6
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-02-12 17:39:23 +0700

    feat(bricks): implement pre-generation logic for data source selection

--- bricks/data_source/hooks/pre_gen.dart
+++ bricks/data_source/hooks/pre_gen.dart
@@ -1,5 +1,18 @@
 import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // TODO: add pre-generation logic.
+void run(HookContext context) async {
+  _runChoose;
+}
+
+void _runChoose(HookContext context) async {
+  final type = context.vars['data_source_type'];
+  final progress = context.logger.progress('Generate data source...');
+
+  context.vars = {
+    ...context.vars,
+    'use_local': type == 'local',
+    'use_remote': type == 'remote',
+  };
+
+  progress.complete();
 }
