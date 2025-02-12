4e2403d9a040b641d80cd1ca24b5b093afa9479f
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-02-12 11:52:34 +0700

    refactor(bricks): moved to cores brick folder
    moved `di`, `env`, `failure`, `modules`, `network`, `storage` to cores brick folder

---
+++ bricks/cores/__brick__/lib/cores/network/network_interceptor.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:injectable/injectable.dart';
+
+/// {@template network_interceptor}
+/// A class that will be used to intercept the network request.
+/// {@endtemplate}
+@lazySingleton
+final class NetworkInterceptor extends QueuedInterceptorsWrapper {
+  /// {@macro network_interceptor}
+  const NetworkInterceptor();
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+    // TODO: implement onRequest
+    super.onRequest(options, handler)
+  }
+
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    // TODO: implement onResponse
+    super.onResponse(response, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    // TODO: implement onError
+    super.onError(err, handler);
+  }
+}
--- bricks/cores/hooks/post_gen.dart
+++ bricks/cores/hooks/post_gen.dart
@@ -8,17 +8,23 @@ void run(HookContext context) async {
   await _runDartFix(context);
 }
 
-
 Future<void> _runAddDependencies(HookContext context) async {
   final progress = context.logger.progress('Adding dependencies...');
 
+  await Process.run('flutter', ['pub', 'add', 'dio']);
+  await Process.run('flutter', ['pub', 'add', 'envied']);
   await Process.run('flutter', ['pub', 'add', 'freezed_annotation']);
+  await Process.run('flutter', ['pub', 'add', 'flutter_secure_storage']);
+  await Process.run('flutter', ['pub', 'add', 'fpdart']);
+  await Process.run('flutter', ['pub', 'add', 'get_it']);
+  await Process.run('flutter', ['pub', 'add', 'injectable']);
   await Process.run('flutter', ['pub', 'add', '--dev', 'build_runner']);
+  await Process.run('flutter', ['pub', 'add', '--dev', 'envied_generator']);
   await Process.run('flutter', ['pub', 'add', '--dev', 'freezed']);
+  await Process.run('flutter', ['pub', 'add', '--dev', 'injectable_generator']);
 
   progress.complete();
 }
-
 
 Future<void> _runDartFormat(HookContext context) async {
   final progress = context.logger.progress('Running dart format...');
