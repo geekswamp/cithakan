3087cef10ff69281a583cf42481376132ed0c9de
Ahmad Rifa'i <personal.rifai@gmail.com>
2025-02-13 16:47:45 +0700

    feat(bloc): enhance dynamic success state and return type options

--- bricks/bloc/__brick__/lib/presentations/{{#use_cubit}}cubits{{\use_cubit}}/{{name.snakeCase()}}/{{name.snakeCase()}}_state.dart
+++ bricks/bloc/__brick__/lib/presentations/{{#use_cubit}}cubits{{\use_cubit}}/{{name.snakeCase()}}/{{name.snakeCase()}}_state.dart
@@ -1,10 +1,3 @@
 part of '{{name.snakeCase()}}_cubit.dart';
 
-@freezed
-sealed class {{name.pascalCase()}}State with _${{name.pascalCase()}}State {
-  // TODO: Please customize your state if needed.
-  const factory {{name.pascalCase()}}State.initial() = _{{name.pascalCase()}}Initial;
-  const factory {{name.pascalCase()}}State.inProgress() = _{{name.pascalCase()}}LoadInProgress;
-  const factory {{name.pascalCase()}}State.success({{name.pascalCase()}} data, [@Default('') String message]) = _{{name.pascalCase()}}LoadSuccess;
-  const factory {{name.pascalCase()}}State.failure(String error) = _{{name.pascalCase()}}LoadFailure;
-}
+{{> state }}
--- bricks/bloc/hooks/pre_gen.dart
+++ bricks/bloc/hooks/pre_gen.dart
@@ -6,12 +6,16 @@ void run(HookContext context) async {
 
 Future<void> _runChoose(HookContext context) async {
   final type = context.vars['bloc_type'];
+  final returnType = context.vars['return_type'];
   final progress = context.logger.progress('Generate data source...');
 
   context.vars = {
     ...context.vars,
     'use_bloc': type == 'bloc',
     'use_cubit': type == 'cubit',
+    'use_single': returnType == 'single',
+    'use_list': returnType == 'list',
+    'use_none': returnType == 'none',
   };
 
   progress.complete();
